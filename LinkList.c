7a501c041a97137133042de5a542791743e0c905
Lewis2333 <61671596+Lewis2333@users.noreply.github.com>
2024-08-07 13:35:38 +0800

    Update LinkList.c
    单链表链式表示和实现
    包括：节点的增删改查

--- LinkList.c
+++ LinkList.c
@@ -11,6 +11,79 @@ void PrintList(LinkList p){
         printf( "%d",p->data);
     }
 }
+//按序列查找数据
+void ResearchIndex(LinkList *L,int index){
+    int i=0;
+
+    LNode *p;
+    p = *L;
+
+    for (;p && i < index; ++i) {
+        p=p->next;
+    }
+    if(!p||i>index){
+        printf("您输入的位置不合法！");
+        return;
+    }
+    printf("该位置的数据是%d",p->data);
+}
+//按值查找序列
+void ReaseachData(LinkList *L,int data){
+    int i = 0;
+    LNode *p;
+    p = *L;
+    for (;p && p->data != data ; ++i) {
+        p=p->next;
+    }
+    if(p){
+        printf("该数据的位置是%d",i);
+        return;
+    }else{
+        printf("未找到数据或序列非法");
+        return;
+    }
+
+}
+//插入数据
+void InsertData(LinkList *L,int index,int data){
+    LinkList p;
+    p = (*L);
+    int i =0;
+    for (;p && i < index - 1; ++i) {
+        p = p->next;
+    }
+    if(!p || i>index){
+        printf("您输入的位置不合法！");
+        return;
+    }
+    LNode *s = (LinkList) malloc(sizeof(LNode));
+    s->next = p->next;
+    p->next = s;
+    s->data = data;
+    printf("插入成功！");
+    PrintList(*L);
+
+}
+//删除数据
+void DeleteData(LinkList *L,int index){
+    int i = 0;
+    LNode *s;
+    s=(*L);
+    for (;s->next && i < index -1; ++i) {
+        s =s->next;
+    }
+    if(!(s->next) || i> index - 1){
+        printf("您输入的位置不合法！");
+        return;
+    }
+    LNode *p = (LinkList) malloc(sizeof(LNode));
+    p->next = s->next->next;
+    free(s->next);
+    s->next = p->next;
+    printf("删除成功！");
+    PrintList(*L);
+}
+//头插法初始化链表
 void CreatList_Head(LinkList *p,int times){
     //创建头节点
     *p = (LinkList) malloc(sizeof(LNode));
@@ -26,6 +99,7 @@ void CreatList_Head(LinkList *p,int times){
     printf("头插法成功！");
     PrintList(*p);
 }
+//尾插法初始化链表
 void CreatList_Tail(LinkList *L,int times){
     //创建头节点
     LinkList p;
@@ -48,5 +122,9 @@ int main() {
     LinkList p;
     CreatList_Head(&p, TIMES);
     CreatList_Tail(&p,TIMES );
+    InsertData(p,2,999);
+    DeleteData(p,2);
+    ResearchIndex(p,1);
+    ReaseachData(p,1);
     return 0;
 }
